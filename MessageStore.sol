cbc18d0b78d529cd4a484ffafd59ddc293b1e089
Gerard Persoon <mail@gpersoon.com>
2026-03-24 15:15:53 +0100

    Update MessageStore.sol

--- MessageStore.sol
+++ MessageStore.sol
@@ -1,32 +1,45 @@
-// Reconstruction - 3 of 8 function selectors remain unidentified
+// Reconstruction 
 // Verified via deployment transaction (creation tx runtime == on-chain code)
+pragma solidity >=0.1.0 <0.4.0;
+pragma experimental ABIEncoderV2;
+
 contract MessageStore {
     struct Message {
         uint64 timestamp;
         string content;
     }
 
-    uint nonce;
-    mapping(address => mapping(bytes32 => Message)) messages;
+    mapping(address => mapping(bytes32 => Message)) public messages;
     mapping(address => bytes32[]) inbox;
+    uint256 public nonce;
 
-    function sendMessage(address recipient, string message) {
-        nonce++;
-        bytes32 key = bytes32(block.timestamp);
-        messages[recipient][key].timestamp = uint64(block.timestamp);
-        messages[recipient][key].content = message;
-        inbox[recipient].push(key);
+    function getMessageHashes() public returns (bytes32[]) {
+        return inbox[msg.sender];
     }
 
-    function getMessageContents(bytes32 key) constant returns (string) {
-        return messages[msg.sender][key].content;
+    function hashes(address account, bytes32 amount) public returns (bytes32) {
+        return inbox[account][amount];
     }
 
-    // selector 0xfe1e3eca
-    function deleteMessage(bytes32 key) {
+    function getMessageTime(bytes32 key) public returns (uint64) {
+        return messages[msg.sender][key].timestamp;
+    }
+
+    function getMessageContents(bytes32 key) public returns (Message) {
+        return messages[msg.sender][key];
+    }
+
+    function sendMessage(address recipient, string message) public {
+        nonce = nonce + 1;
+        messages[recipient][nonce].timestamp = 4;
+        messages[recipient][nonce].content = message;
+        inbox[recipient].push(nonce);
+    }
+
+    function deleteMessage(bytes32 key) public {
         messages[msg.sender][key].timestamp = 0;
-        delete messages[msg.sender][key].content;
-        for (uint i = 0; i < inbox[msg.sender].length; i++) {
+		delete messages[msg.sender][key].content;
+		for (uint256 i = 0; i < inbox[msg.sender].length; i++) {
             if (inbox[msg.sender][i] == key) {
                 delete inbox[msg.sender][i];
             }
